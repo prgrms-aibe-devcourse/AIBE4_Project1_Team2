5fb4ba8377447ece2e0d1514ffc4d22f36e75994
khyun722 <kungilove1999@gmail.com>
2025-10-15 00:55:23 +0900

    feat:마이페이지 모달 수정

--- frontend/saved-ai-plan/saved-ai-plan.js
+++ frontend/saved-ai-plan/saved-ai-plan.js
@@ -1,1 +1,4 @@
-const btnReview = document.getElementById
+const btnReview = document.getElementById('btnReview');
+btnReview.addEventListener("click", () => {
+    window.location.href = '../review-form/review-form.html';
+})
