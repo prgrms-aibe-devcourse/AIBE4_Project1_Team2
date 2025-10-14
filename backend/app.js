91ea193c4e3d550438f2ab84f8aeccdfbb6c7c9b
jivvoor <wldn040839@gmail.com>
2025-10-14 15:50:32 +0900

    리뷰 목록 조회, 상세 조회, 검색 API 구현

--- backend/app.js
+++ backend/app.js
@@ -21,3 +21,6 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+const reviewRoutes = require('./routes/reviewRoutes');
+app.use('/reviews', reviewRoutes);
---
+++ backend/routes/reviewRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const reviewController = require('../controllers/reviewController');
+
+// 검색은 /search를 먼저 정의 (/:id보다 앞에 위치해야 함)
+router.get('/search', reviewController.searchReviews);
+
+// 리뷰 조회
+router.get('/', reviewController.getAllReviews);
+router.get('/:id', reviewController.getReviewById);
+
+module.exports = router;
