dbe5e091d7ebd2ede3caca022832d922a36eb0df
JunHyun <junhyun1001@gmail.com>
2025-10-15 14:50:09 +0900

    api 경로 수정

--- backend/routes/planRoutes.js
+++ backend/routes/planRoutes.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const planController = require("../controllers/planController");
 
 // POST 요청이 /plans 경로로 들어오면 planController.createPlan 함수를 실행
-router.post("/plans", planController.createPlan);
+router.post("/plan", planController.createPlan);
 
 // AI 출력 결과 저장
-router.post("/schedules", planController.savePlan);
+router.post("/plan-save", planController.savePlan);
 
 // 내가 저장한 일정 조회
-router.get("/mypage/schedules", planController.getMyPlans);
+router.get("/my-plans", planController.getMyPlans);
 
 module.exports = router;
